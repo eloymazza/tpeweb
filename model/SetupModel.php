2ee70bc89b3dac342ae6bb9259c1c2dc57838e54
Eloy Mazza <eloymazza.ra@gmail.com>
2017-10-18 22:36:15 -0300

    Db se crea sola

--- model/SetupModel.php
+++ model/SetupModel.php
@@ -2,8 +2,11 @@
 class SetupModel
 {
   function runSetupScript($host, $dbname, $user, $pass){
-    $conexion = 'mysql:host='.$host.';dbname='.$dbname.';charset=utf8';
-    $db = new PDO($conexion, $user, $pass);
+
+    $mysql = new PDO("mysql:host=".$host.";dbname=", $user, $pass);
+    $createDB = $mysql->prepare("CREATE DATABASE IF NOT EXISTS $dbname");
+    $createDB->execute();
+    $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
     $file = fopen("db_test.sql", "r") or die("Unable to open database.sql file!");
     $sql = fread($file,filesize("db_test.sql"));
     fclose($file);
